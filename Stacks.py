292a08d91e89409bc7048fd7f95cb43fe7c9976d
Luis Francisco Contreras Gonzalez <51728784+LuisContrerasGlz@users.noreply.github.com>
2021-07-13 01:53:30 -0500

    Stacks section

--- Stacks.py
+++ Stacks.py
@@ -14,3 +14,113 @@
 # Depending on the implementation, the top of the stack is equivalent to the head node of a linked list and the bottom of the stack is equivalent to the tail node.
 # A constraint that may be placed on a stack is its size. This is done to limit and quantify the resources the data structure will take up when it is “full”.
 # Attempting to push data onto an already full stack will result in a stack overflow. Similarly, if you attempt to pop data from an empty stack, it will result in a stack underflow.
+
+# We also need to consider the stack’s size and tweak our methods a bit so that our stack does not “overflow”.
+from node import Node
+
+# Add your Stack class below:
+class Stack:
+  def __init__(self):
+    self.top_item = None
+    
+  def peek(self):
+    return self.top_item.get_value()
+  
+# The stack’s push() and pop() methods are our tools to add and remove items from it. pop() additionally returns the value of the item it is removing. 
+# Keep in mind that we can only make modifications to the top of the stack.
+
+from node import Node
+
+class Stack:
+  def __init__(self):
+    self.top_item = None
+  
+  # Define your push() and pop() methods below:
+
+  def push(self, value):
+    item = Node(value)
+    item.set_next_node(self.top_item)
+    self.top_item = item
+
+  def pop(self):
+    item_to_remove = self.top_item
+    self.top_item = item_to_remove.get_next_node()
+    return item_to_remove.get_value()
+  
+  def peek(self):
+    return self.top_item.get_value()
+ 
+# With stacks, size matters. If we’re not careful, we can accidentally over-fill them with data. 
+# Since we don’t want any stack overflow, we need to go back and make a few modifications to our methods that help us track and limit the stack size so we can keep our stacks healthy.
+
+from node import Node
+
+class Stack:
+  def __init__(self, limit=1000):
+    self.top_item = None
+    self.limit = limit
+    self.size = 0
+  
+  def push(self, value):
+    item = Node(value)
+    item.set_next_node(self.top_item)
+    self.top_item = item
+
+  def pop(self):
+    if self.size > 0:
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      # Decrement the stack size here:
+      self.size -= 1
+      return item_to_remove.get_value()
+    else:
+      print("This stack is totally empty.")
+  
+  def peek(self):
+    if self.size > 0:
+      return self.top_item.get_value()
+    else:
+      print("Nothing to see here!")
+ 
+from node import Node
+
+class Stack:
+  def __init__(self, limit=1000):
+    self.top_item = None
+    self.size = 0
+    self.limit = limit
+  
+  def push(self, value):
+    if self.has_space():
+      
+    	item = Node(value)
+    	item.set_next_node(self.top_item)
+    	self.top_item = item
+    # Increment stack size by 1 here:
+    	self.size += 1
+    else:
+      print("All out of space!")
+        
+
+  def pop(self):
+    if not self.is_empty():
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      return item_to_remove.get_value()
+    else:
+      print("This stack is totally empty.")
+  
+  def peek(self):
+    if not self.is_empty():
+	    return self.top_item.get_value()
+    else:
+      print("Nothing to see here!")
+      
+  # Define has_space() and is_empty() below:
+  def has_space(self):
+    return self.limit > self.size
+  
+  def is_empty(self):
+    return self.size == 0
+ 
